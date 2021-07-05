d008bf18b1c1af4b9644da377e581aef35588e38
Leonid Gorunov <leonid.g@siteground.com>
2021-07-05 11:02:15 +0300

    Make it more readable

--- loops.php
+++ loops.php
@@ -2,15 +2,20 @@
 
 
 for ($x=0; $x < 101; $x ++) {
-  echo $x;
   if ($x % 3 === 0 && $x % 5 === 0) {
+    echo $x;
     echo "FooBar";
+    echo "<br>"
   }
   if ($x % 3 === 0) {
+    echo $x;
   echo "Foo";
+  echo "<br>"
   }
   if ($x % 5 === 0) {
+    echo $x;
   echo "Bar";
+  echo "<br>"
   }
 
 }
